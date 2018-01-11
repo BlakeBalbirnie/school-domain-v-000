77729d0bd6b9a854d6e9fadf67de08c003912018
Blake Balbirnie <blakebalbirnie@gmail.com>
2018-01-11 16:01:00 +0000

    automatically backed up by learn

--- lib/school.rb
+++ lib/school.rb
@@ -1,6 +1,27 @@
-school = School.new("Bayside High School")
+require 'pry'
+class School
+  attr_accessor :roster, :name, :grade
+  attr_reader :school
 
-school.add_student("AC Slater", 9)
-school.add_student("Kelly Kapowski", 10)
-school.add_student("Screech", 11)
-school.roster
+  def initialize(school)
+    @school = school
+    @roster = {}
+  end
+
+  def add_student(name,grade)
+    roster[grade] ||= []
+    roster[grade] << name
+  end
+
+  def grade(grade)
+    roster[grade]
+  end
+
+  def sort
+    new_hash = {}
+    roster.each do |k,v|
+      new_hash[k] = v.sort
+    end
+    new_hash
+  end
+end
