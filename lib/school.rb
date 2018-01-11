d61f463367b34c2494f59e9ed52d8a4c55e71516
Blake Balbirnie <blakebalbirnie@gmail.com>
2018-01-11 16:00:10 +0000

    automatically backed up by learn

--- lib/school.rb
+++ lib/school.rb
@@ -1,1 +1,6 @@
-# code here!
+school = School.new("Bayside High School")
+
+school.add_student("AC Slater", 9)
+school.add_student("Kelly Kapowski", 10)
+school.add_student("Screech", 11)
+school.roster
